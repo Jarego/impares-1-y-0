f1b571d368d8b1824c6f15327c1cbc01428f2f94
Jarego <jaregocreepy@hotmail.com>
2019-04-28 01:35:54 -0500

    Add files via upload

---
+++ impar 0 y 1/impar 0 y 1/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+    Dim var1 As String
+    Dim var2 = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text += " "
+        q0()
+    End Sub
+    Sub q0()
+        If TextBox1.Text(var2) = "1" Then
+            var1 += "1"
+            var2 += 1
+            Label1.Text = var1
+            MsgBox("1:1,D")
+            q1()
+        ElseIf TextBox1.Text(var2) = "0" Then
+            var1 += "0"
+            var2 += 1
+            Label1.Text = var1
+            MsgBox("0:0,D")
+            q1()
+
+        End If
+    End Sub
+    Sub q1()
+        If TextBox1.Text(var2) = "1" Then
+            var1 += "1"
+            var2 += 1
+            Label1.Text = var1
+            MsgBox("1:1,D")
+            q1()
+        ElseIf TextBox1.Text(var2) = "0" Then
+            var1 += "0"
+            var2 += 1
+            Label1.Text = var1
+            MsgBox("0:0,D")
+            q1()
+        ElseIf TextBox1.Text(var2) = " " Then
+
+            MsgBox("Cadena aceptada")
+
+        End If
+    End Sub
+End Class
